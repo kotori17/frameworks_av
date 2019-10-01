b6b2b02ac0e1b2917ddfb2bc6f19dc190fbd4875
Pierre-Hugues Husson <phh@phh.me>
2019-10-01 13:35:49 +0200

    Add (partial, cam id is hardcoded) support for Asus ZF6 motor camera
    Change-Id: Iea6e1370780a1d16f728748d1d948d092532d8fe

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -29,6 +29,7 @@
 #include <fstream>
 #endif
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <inttypes.h>
 #include <pthread.h>
 
@@ -135,6 +136,7 @@ Mutex CameraService::sProxyMutex;
 sp<hardware::ICameraServiceProxy> CameraService::sCameraServiceProxy;
 
 CameraService::CameraService() :
+        mPhysicalFrontCamStatus(false),
         mEventLog(DEFAULT_EVENT_LOG_LENGTH),
         mNumberOfCameras(0),
         mSoundRef(0), mInitialized(false) {
@@ -1581,6 +1583,7 @@ Status CameraService::connectHelper(const sp<CALLBACK>& cameraCb, const String8&
             mServiceLock.lock();
         } else {
             // Otherwise, add client to active clients list
+            physicalFrontCam(cameraId == "1");
             finishConnectLocked(client, partial);
 
             sp<ICameraMotor> cameraMotor = ICameraMotor::getService();
@@ -1594,6 +1597,27 @@ Status CameraService::connectHelper(const sp<CALLBACK>& cameraCb, const String8&
     // destructor (can be at the end of the call)
     device = client;
     return ret;
+}
+
+void CameraService::physicalFrontCam(bool on) {
+    if(on == mPhysicalFrontCamStatus) return;
+    mPhysicalFrontCamStatus = on;
+
+    if(access("/dev/asusMotoDrv", F_OK) == 0) {
+        int pid = fork();
+        if(pid == 0) {
+            const char* cmd[] = {
+                "/system/bin/asus-motor",
+                "0",
+                NULL
+            };
+            cmd[1] = on ? "0" : "1";
+            execve("/system/bin/asus-motor", (char**)cmd, environ);
+            _exit(1);
+        } else {
+            waitpid(pid, NULL, 0);
+        }
+    }
 }
 
 Status CameraService::setTorchMode(const String16& cameraId, bool enabled,
@@ -2481,6 +2505,8 @@ binder::Status CameraService::BasicClient::disconnect() {
     if (cameraMotor != nullptr) {
         cameraMotor->onDisconnect(mCameraIdStr.string());
     }
+
+    sCameraService->physicalFrontCam(false);
 
     sCameraService->removeByClient(this);
     sCameraService->logDisconnected(mCameraIdStr, mClientPid, String8(mClientPackageName));
